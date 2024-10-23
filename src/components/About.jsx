1434129aef67e25113a92eb30131fe644d93e0d1
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-23 21:02:03 +0530

    added styling and contents

---
+++ src/components/About.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+  return <div>
+    <div className="bg-zinc-100 w-1/2 m-auto mt-10">
+      <h1 className="text-5xl mb-2">About</h1>
+      <p className="mb-2">Consequat deserunt sunt sint et qui nulla eu dolor aliqua ea irure.Consequat deserunt sunt sint et qui nulla eu dolor aliqua ea irure.</p>
+      <button className="p-3 bg-red-300 rounded-md">Explore More</button>
+    </div>
+
+  </div>;
+};
+
+export default About;
