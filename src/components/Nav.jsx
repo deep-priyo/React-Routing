42f398af3936ceccc4684ff8a6a05aa0733593c1
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-23 21:54:59 +0530

    styling using style property of Navlink

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,9 +3,11 @@ import { Link, NavLink } from "react-router-dom";
 const nav = () => {
     return <div>
         <nav className="flex gap-20 justify-center py-1  text-white bg-white border-gray-200 dark:bg-gray-900 ">
-            <NavLink style={(e) => { return { color: e.isActive ? "tomato" : "" }; }} to="/">Home</NavLink>
-            <NavLink style={(e) => { return { color: e.isActive ? "#f0ab8d" : "" }; }} to="/user">User</NavLink>
-            <NavLink style={(e) => { return { color: e.isActive ? "#f0ab8d" : "" }; }} to="/about">About</NavLink>
+
+            <NavLink style={(e) => { return { color: e.isActive ? "tomato" : "", fontWeight: e.isActive ? "bold" : "", fontSize: e.isActive ? "18px" : "" }; }} to="/">Home</NavLink>
+            <NavLink style={(e) => { return { color: e.isActive ? "#f0ab8d" : "", fontWeight: e.isActive ? "bold" : "", fontSize: e.isActive ? "18px" : "" }; }} to="/user">User</NavLink>
+            <NavLink style={(e) => { return { color: e.isActive ? "#f0ab8d" : "", fontWeight: e.isActive ? "bold" : "", fontSize: e.isActive ? "18px" : "" }; }} to="/about">About</NavLink>
+
         </nav></div>;
 };
 
