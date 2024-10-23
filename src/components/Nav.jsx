bc9fd655dd530d3a8a36cec0ff1c48a6bf5b1c54
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-10-23 22:10:51 +0530

    styling using className of Navlink

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -4,11 +4,17 @@ const nav = () => {
     return <div>
         <nav className="flex gap-20 justify-center py-1  text-white bg-white border-gray-200 dark:bg-gray-900 ">
 
-            <NavLink style={(e) => { return { color: e.isActive ? "tomato" : "", fontWeight: e.isActive ? "bold" : "", fontSize: e.isActive ? "18px" : "" }; }} to="/">Home</NavLink>
-            <NavLink style={(e) => { return { color: e.isActive ? "#f0ab8d" : "", fontWeight: e.isActive ? "bold" : "", fontSize: e.isActive ? "18px" : "" }; }} to="/user">User</NavLink>
-            <NavLink style={(e) => { return { color: e.isActive ? "#f0ab8d" : "", fontWeight: e.isActive ? "bold" : "", fontSize: e.isActive ? "18px" : "" }; }} to="/about">About</NavLink>
+            <NavLink className={(e) => {
+                return [e.isActive ? "text-red-500" : "",
+                e.isActive ? "font-bold" : ""].join(" ")
+            }} to="/">Home</NavLink>
+            <NavLink className={(e) => {
+                return [e.isActive ? "text-red-500" : "",
+                e.isActive ? "text-bold" : ""].join(" ")
+            }} to="/user">User</NavLink>
+            <NavLink style={(e) => { return { color: e.isActive ? "#f0ab8d" : "", fontWeight: e.isActive ? "bold" : "" }; }} to="/about">About</NavLink>
 
-        </nav></div>;
+        </nav></div >;
 };
 
 export default nav;
