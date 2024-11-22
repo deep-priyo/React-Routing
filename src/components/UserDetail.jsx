89edaca7a2df95885c5d68e83ec02958b15338d8
Priyadeep Mullick <129691885+deep-priyo@users.noreply.github.com>
2024-11-23 00:34:53 +0530

    Handling user details

---
+++ src/components/UserDetail.jsx
@@ -0,0 +1,19 @@
+import { useParams } from "react-router-dom";
+
+const UserDetail = () => {
+    const { name } = useParams();
+
+    return (
+
+
+        <div className="bg-zinc-100 w-1/2 m-auto mt-10">
+            <h1 className="text-5xl mb-2">User Detail</h1>
+            <h1 className="text-3xl mt-2 mb-2 text-orange-400">Hey, {name}</h1>
+            <p className="mb-2">Consequat deserunt sunt sint et qui nulla eu dolor aliqua ea irure.Consequat deserunt sunt sint et qui nulla eu dolor aliqua ea irure.</p>
+            <button className=" p-3 bg-red-300 rounded-md">Go back</button>
+        </div>
+
+    );
+};
+
+export default UserDetail;
--- src/utils/Routing.jsx
+++ src/utils/Routing.jsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import Home from "../components/Home";
 import User from "../components/User";
 import About from "../components/About";
+import UserDetail from "../components/UserDetail";
 const Routing = () => {
     return (
         <div>
@@ -9,6 +10,7 @@ const Routing = () => {
                 <Route path="/" element={<Home />} />
                 <Route path="/user" element={<User />} />
                 <Route path="/about" element={<About />} />
+                <Route path="/user/:name" element={<UserDetail />} />
             </Routes>
         </div>
     );
